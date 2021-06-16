0acdc4895ed4120077be7c70627feaf0668781f7
Isadora Moro <84686323+isadoramoro@users.noreply.github.com>
2021-06-16 01:17:02 +0000

    Inseridas as configurações de elipse

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ import Text.Printf
 
 type Point     = (Float,Float)
 type Rect      = (Point,Float,Float)
+type Ellipse    = (Point,Float,Float)
 
 -------------------------------------------------------------------------------
 -- String inicial do SVG
@@ -20,15 +21,17 @@ rotation :: (Int,Int,Int) -> String
 rotation (a, x, y) = printf "rotate(%d,%d,%d)" a x y
 
 angRotation :: Int -> [(Int,Int,Int)]
-angRotation n = [(i, 400, 300) | i <- take n(iterate (+20) a)]
+angRotation n = [(ang, 400, 300) | ang <- take n(iterate (+a) a)]
   where a = 20
 -------------------------------------------------------------------------------
-strkStyle :: (Int,Int,Int) -> String
-strkStyle (r,g,b) = printf "stroke:rgb(%d,%d,%d);fill-opacity:0.0;" r g b
+strkStyle :: (Int,Int,Int, Float) -> String
+strkStyle (r,g,b,o) = printf "stroke:rgb(%d,%d,%d);fill-opacity:%f;" r g b o
 
-newPalette :: Int -> [(Int,Int,Int)]
-newPalette n = take n [(a+3*i, a+2*i, a)| i <- take n(iterate (+20) a)]
-  where a = 6
+newPalette :: Int -> [(Int,Int,Int, Float)]
+newPalette n = take n [((div a 2), a, i, o)| i <- take n(iterate (+b) 0)]
+  where a = 255
+        b = 25
+        o = 0.0
 -------------------------------------------------------------------------------
 genRects :: Int -> [Rect]
 genRects n = replicate n ((x,y), w, h)
@@ -37,20 +40,42 @@ genRects n = replicate n ((x,y), w, h)
         w = 150
         h = 150
 -------------------------------------------------------------------------------
-svgElements :: [(Rect, String, String)] -> String
+svgElements :: [((Point,Float,Float), String, String)] -> String
 svgElements elements = concat $ map svgRect elements
+-------------------------------------------------------------------------------
+svgEllipse :: (Ellipse, String) -> String
+svgEllipse (((cx,cy), rx, ry), style) =
+  printf "<ellipse cx='%f' cy='%f' rx='%f' ry='%f' style='%s' />\n" cx cy rx ry style
 
---teste:
---concat(map svgRect (zipWith3 (,,) (genRects 2) (map rotation (angRotation 2)) (map strkStyle (newPalette 2)))) --OK
+elPalette :: Int -> [(Int,Int,Int,Float)]
+elPalette n = take n [(a+i, a+2*i, a+3*i, o) | i <- take n(iterate (+10) a)]
+  where a = 6
+        o = 0.0
+
+genEllip :: Int -> [Ellipse]
+genEllip n = replicate n ((cx,cy), rx, ry)
+  where cx = 400
+        cy = 300
+        rx = 212
+        ry = 212
+
+svgElements1 :: [((Point,Float,Float), String)] -> String
+svgElements1 elements = concat $ map svgEllipse elements
 -------------------------------------------------------------------------------
 main :: IO()
 main = do 
   writeFile "trab1.svg" $ svgstrs
-  where svgstrs = svgBegin w h ++ part1 ++ svgEnd
+  where svgstrs = svgBegin w h ++ part1 ++ part2 ++ svgEnd
         part1 = svgElements (zipWith3 (,,) rects transform style)
         rects = genRects nrects
         transform = (map rotation (angRotation nrects))
         style = (map strkStyle palette)
         palette = newPalette nrects
         nrects = 18
+
+        part2 = svgElements1 (zipWith (,) ellip (map strkStyle color))
+        ellip = genEllip nellip
+        color = elPalette nellip
+        nellip = 3
+
         (w,h) = (800,600)
